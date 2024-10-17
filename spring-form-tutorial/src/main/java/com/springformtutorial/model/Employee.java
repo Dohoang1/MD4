7430ad199112d3ac4d25f12be54bbcbc6cc7e2be
Hoang <ihngno1@gmail.com>
2024-10-17 16:40:21 +0700

    hoang update

---
+++ spring-form-tutorial/src/main/java/com/springformtutorial/model/Employee.java
@@ -0,0 +1,35 @@
+package com.springformtutorial.model;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String contactNumber;
+
+    // Constructor mặc định
+    public Employee() {}
+
+    // Getters và setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
